a8f100abfa4bf4cf98947f1c1a93b424b9fa2348
unknown <dawid.krynski@student.uj.edu.pl>
2024-11-16 15:32:07 +0100

    WIP index template

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -16,20 +16,22 @@ class TopicController {
       const topic = await Topic.findById(req.params.id);
       if (!topic) return res.status(404).send("Topic not found");
 
-      topic.userId = req.session.userId; // Add this line to set userId
+      topic.userId = req.session.userId;
 
       const prerequisites = await topic.getPrerequisites();
       const progress = await topic.getProgress(req.session.userId);
       const exerciseGroups = await topic.getExerciseGroups();
 
-      const templateName = topic.name.toLowerCase().replace(/\s+/g, "_");
-      res.render(`topics/${templateName}/index`, {
-        topic,
-        prerequisites,
-        progress,
-        exerciseGroups,
-        req,
-      });
+      res.render(
+        `topics/${topic.name.toLowerCase().replace(/\s+/g, "_")}/index`,
+        {
+          topic,
+          prerequisites,
+          progress,
+          exerciseGroups,
+          req,
+        }
+      );
     } catch (err) {
       console.error(err);
       res.status(500).send("Error loading topic");
